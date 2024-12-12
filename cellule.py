d0182cb188ca33cd898893f6e175c232913c47f2
chloe-lg <cloclo.lg.277gmail.com>
2024-12-12 12:04:47 +0100

    add

---
+++ cellule.py
@@ -0,0 +1,15 @@
+class Cellule:
+    def __init__(self, vivante, vivera, x, y, g):
+        self.vivante = vivante
+        self.vivera = vivera
+        self.x = x
+        self.y = y
+        self.g = g
+
+    def calcule_etat_futur():
+        """Détermine l’état à venir de la cellule (attribut vivra)"""
+        pass
+
+    def update():
+        """Met à jour la cellule en affectant l’état à venir vivra à l’attribut actuel vivante"""
+        pass
