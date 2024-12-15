038cd63aba7a4c595aac422598460e4493647aaa
0q3i <nicolas521janini@gmail.com>
2024-12-15 21:00:30 +0100

    2/3 retouche et ajout

--- grille.py
+++ grille.py
@@ -1,4 +1,5 @@
 from math import *
+from cellule import *
 
 class Grille:
 
@@ -6,6 +7,10 @@ class Grille:
         self.longuer = l #loguer d'une case
         self.hauteur = h #hauteur d'une case
         self.lst_c = lst_cellule #list des cellul vivante
+
+    def grille(self):
+        #desine une geille cet methode elle va pt pas excite dans le futur
+        pass
 
     def tour(self):
         """ne renvois rien, modifi g dans la class cellul"""
@@ -17,12 +22,27 @@ class Grille:
 
     def voisin(self):
         """renvois un int, compte le nombre de voisin a une case donne"""
-        pass
+        voisin_tottale = -1
+        for y in range(3):
+            for x in range(3):
+                pass
 
-    def _ajoute(self):
+    def _ajoute(self, cellul):
         """renvois rein, elle ajoute les cellul vivante a lst_c"""
-        pass
+        self.lst_c.append(cellul)
 
     def _remove(self):
         """renvois rien, elle enleve les celluls de lst_c"""
-        pass
+        for i in range(len(self.lst_c)):
+            if not self.lst_c[i].vivante:
+                self.lst_c[i] = None
+        self.lst_c = _nouv_lst(self.lst_c)
+        
+def _nouv_lst(lst):
+    """renvois une nouvelle list avec que des cellul vivante, On suppose que la list est composer uniquement de cellul"""
+    nouv_lst = [] 
+    for elm in lst:
+        if elm is not None:
+            nouv_lst.append(elm)
+    return nouv_lst
+            
