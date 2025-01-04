f3d12f63e2bd84ca1a4f48155851cbf5144ff228
0q3i <nicolas521janini@gmail.com>
2025-01-04 18:06:40 +0100

    fini totalement import ciculaire

--- grille.py
+++ grille.py
@@ -7,13 +7,14 @@ class Grille:
         self.longuer = l
         self.hauteur = h
         self.matrice = [[c.Cellule(False, False, x_c, x_y, self) for x_c in range(x)] for x_y in range(y)]
-        self.lst_c = []
+        self.lst_c = []  # Liste des cellules vivantes
 
     def tour(self):
         """Met à jour l'état des cellules"""
-        for c in self.lst_c:
-            c.calcule_etat_futur()
-            c.update()
+        for c_y in self.matrice:  # Parcours de toute la matrice
+            for c_x in c_y:
+                c_x.calcule_etat_futur()  # Calcule l'état futur de chaque cellule
+                c_x.update()  # Met à jour l'état actuel de chaque cellule
 
     def voisin(self, cellule):
         """Renvoie la liste des voisins d'une cellule"""
@@ -34,23 +35,24 @@ class Grille:
 
     def _remove(self):
         """Enlève les cellules mortes"""
-        self.lst_c = [cellule for cellule in self.lst_c if cellule.vivante]
+        self.lst_c = [cellule for lst_cellule in self.lst_c for cellule in lst_cellule if cellule.vivante]
 
 # Test
-A = c.Cellule(True, None, 10, 10, None)
-B = c.Cellule(True, None, 10, 11, None)
-C = c.Cellule(True, None, 20, 20, None)
-
 g = Grille(10, 10, 1, 1)
+
+# Création des cellules et ajout à la grille
+A = c.Cellule(True, None, 10, 10, g)  # Passer l'instance de Grille à la cellule
+B = c.Cellule(True, None, 10, 11, g)
+C = c.Cellule(True, None, 20, 20, g)
+
 g._ajoute(A)
 g._ajoute(B)
 g._ajoute(C)
 
-lst_c_v = g.voisin(c.Cellule(True, None, 11, 10, None))
+# Exemple d'appel à la méthode voisin pour obtenir les voisins de la cellule à (11, 10)
+lst_c_v = g.voisin(c.Cellule(True, None, 11, 10, g))
 for elm in lst_c_v:
-    print(elm)
-
-
+    print(elm)  # Affiche les voisins
 
 
 
